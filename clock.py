5be0a48b3550354dbfc8c492e21eb47cde8fa35b
Bousqi <bousqi@gmail.com>
2016-09-23 09:27:35 +0200

    Updated clock to clean and power off display on exit Added dependency on BME280 Systemd service to start clock on boot

--- clock.py
+++ clock.py
@@ -3,7 +3,6 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 import sys
-sys.path.insert(0, '/usr/local/src/sensors/bme280')
 import bme280
 
 from RPLCD_i2c import CharLCD
@@ -12,44 +11,7 @@ from RPLCD_i2c import cursor, cleared
 from time import strftime, sleep
 from datetime import datetime
 
-try:
-    input = raw_input
-except NameError:
-    pass
-
-try:
-    unichr = unichr
-except NameError:
-    unichr = chr
-
-old_time = 0
-counter = 0
-lcd = CharLCD(address=0x3F, port=1, cols=16, rows=2, dotsize=8)
-
-
-# custom symbols
-lcd.clear()
-top_line = (0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000)
-bottom_line = (0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111)
-both_lines = (0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111)
-dot = (0b00000, 0b00000, 0b00000, 0b11000, 0b11000, 0b00000, 0b00000, 0b00000)
-up_left = (0b00111, 0b01111, 0b01111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111)
-up_right = (0b11100, 0b11110, 0b11110, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111)
-bot_right = (0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11110, 0b11110, 0b11100)
-bot_left = (0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b01111, 0b01111, 0b00111)
-
-
-lcd.create_char(0, top_line)
-lcd.create_char(1, bottom_line)
-lcd.create_char(2, both_lines)
-lcd.create_char(3, dot)
-lcd.create_char(4, up_left)
-lcd.create_char(5, up_right)
-lcd.create_char(6, bot_right)
-lcd.create_char(7, bot_left)
-
 # build big digits
-
 def disp_number(lcd_char, position):
 
     if lcd_char=="0" :
@@ -202,7 +164,7 @@ def clock_dots():
     lcd.write_string(unichr(254))
     sleep(0.5)
 
-def clock_date():
+def clock_date(digits, month, day_name):
     # previous text leaving to left
     shift("left")
     # preparing date buffer
@@ -240,7 +202,7 @@ def clock_temp():
     shift("right")
     sleep(5)
 
-def clock_hour():
+def clock_hour(digits):
     # date leaving to left
     shift("left")
     # preparing hour buffer
@@ -248,42 +210,91 @@ def clock_hour():
     lcd.write_string('                ')
     lcd.cursor_pos = (1, 0)
     lcd.write_string('                ')
-    tens_hour = disp_number(digits[0], 0)
-    hour = disp_number(digits[1], 4)
-    tens_minutes = disp_number(digits[2], 9)
-    minutes = disp_number(digits[3], 13)
+    disp_number(digits[0], 0)
+    disp_number(digits[1], 4)
+    disp_number(digits[2], 9)
+    disp_number(digits[3], 13)
     # hour entering from left
     shift("right")
 
-#main loop
 
-while True:
-    counter += 1
-    new_time = datetime.now().strftime('%H%M%d')
-    month = datetime.now().strftime('%B')
-    day_name = datetime.now().strftime('%A')
-    
-    # time changed, update LCD buffer
-    if new_time!=old_time :
-        digits = str(new_time)
-        tens_hour = disp_number(digits[0], 0)
-        hour = disp_number(digits[1], 4)
-        tens_minutes = disp_number(digits[2], 9)
-        minutes = disp_number(digits[3], 13)
-        old_time = new_time
+def main():
+
+    try:
+        input = raw_input
+    except NameError:
+        pass
+
+    try:
+        unichr = unichr
+    except NameError:
+        unichr = chr
+
+    old_time = 0
+    counter = 0
+
+    # custom symbols
+    lcd.clear()
+    top_line = (0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000)
+    bottom_line = (0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111)
+    both_lines = (0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111)
+    dot = (0b00000, 0b00000, 0b00000, 0b11000, 0b11000, 0b00000, 0b00000, 0b00000)
+    up_left = (0b00111, 0b01111, 0b01111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111)
+    up_right = (0b11100, 0b11110, 0b11110, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111)
+    bot_right = (0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11110, 0b11110, 0b11100)
+    bot_left = (0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b01111, 0b01111, 0b00111)
+
+    lcd.create_char(0, top_line)
+    lcd.create_char(1, bottom_line)
+    lcd.create_char(2, both_lines)
+    lcd.create_char(3, dot)
+    lcd.create_char(4, up_left)
+    lcd.create_char(5, up_right)
+    lcd.create_char(6, bot_right)
+    lcd.create_char(7, bot_left)
+
+    #main loop
+    while True:
+        counter += 1
+        new_time = datetime.now().strftime('%H%M%d')
+        month = datetime.now().strftime('%B')
+        day_name = datetime.now().strftime('%A')
+
+        # time changed, update LCD buffer
+        if new_time!=old_time :
+            digits = str(new_time)
+            tens_hour = disp_number(digits[0], 0)
+            hour = disp_number(digits[1], 4)
+            tens_minutes = disp_number(digits[2], 9)
+            minutes = disp_number(digits[3], 13)
+            old_time = new_time
+
+            cur_hour = int(tens_hour)*10 + int(hour)
+            # enable backlight by night
+            lcd.set_backlight(cur_hour>=19 or cur_hour<9)
+
+        # dots blink
+        clock_dots()
         
-        cur_hour = int(tens_hour)*10 + int(hour)
-        # enable backlight by night
-        lcd.set_backlight(cur_hour>=19 or cur_hour<9)
+        if counter==15 :
+            counter=0
+            # date display
+            clock_date(digits, month, day_name)
+            # temperature display
+            clock_temp()
+            # temperature display
+            clock_hour(digits)
 
-    # dots blink
-    clock_dots()
-    
-    if counter==15 :
-        counter=0
-        # date display 
-        clock_date()
-        # temperature display 
-        clock_temp()
-        # temperature display 
-        clock_hour()
+
+if __name__ == '__main__':
+
+  lcd = CharLCD(address=0x3F, port=1, cols=16, rows=2, dotsize=8)
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd.clear()
+    lcd.set_backlight(False)
+    lcd.home()
